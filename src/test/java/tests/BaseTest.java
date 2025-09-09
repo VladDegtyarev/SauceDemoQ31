76ee910733b8d44a586d1cd0fbb1c224885b78d7
VladDegtyarev <vladdegtyarev3@gmail.com>
2025-09-09 14:50:14 +0300

    add secrets

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -24,8 +24,8 @@ public class BaseTest {
     CartPage cartPage;
     CheckoutPage checkoutPage;
 
-    String user = System.getProperty("user");
-    String password = System.getProperty("password");
+    String user = System.getProperty("user",PropertyReader.getProperty("user"));
+    String password = System.getProperty("password",PropertyReader.getProperty("password"));
 
 
     @Parameters({"browser"})
